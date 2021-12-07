d3e50cf9eefd55747b1ff0c1e8fe19be5a68acf6
Raphael G <raphaelgal@gmail.com>
2021-12-07 19:22:29 +0200

    Initial commit

---
+++ src/main/java/com/udacity/bootstrap/service/DogServiceImp.java
@@ -0,0 +1,33 @@
+package com.udacity.bootstrap.service;
+
+import com.udacity.bootstrap.entity.Dog;
+import com.udacity.bootstrap.repository.DogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DogServiceImp implements DogService{
+    @Autowired
+    DogRepository dogRepository;
+
+    public List<Dog> retrieveDogs() {
+        return (List<Dog>) dogRepository.findAll();
+    }
+
+    public List<String> retrieveDogBreed() {
+        return (List<String>) dogRepository.findAllBreed();
+    }
+
+    public String retrieveDogBreedById(Integer id) {
+        Optional<String> optionalBreed = Optional.ofNullable(dogRepository.findBreedById(id));
+        String breed = optionalBreed.orElseThrow(DogNotFoundException::new);
+        return breed;
+    }
+
+    public List<String> retrieveDogNames() {
+        return (List<String>) dogRepository.findAllName();
+    }
+}
