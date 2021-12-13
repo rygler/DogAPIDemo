ad51d29eda8baf605b06e3febe45852923056b6f
Raphael G <raphaelgal@gmail.com>
2021-12-13 11:23:35 +0200

    Fixed circular dependency error

--- SpringRestAndGraphQLAPI/src/main/java/com/udacity/bootstrap/config/SpringSecurityConfig.java
+++ SpringRestAndGraphQLAPI/src/main/java/com/udacity/bootstrap/config/SpringSecurityConfig.java
@@ -9,11 +9,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -29,13 +29,11 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth)
             throws Exception {
         auth.inMemoryAuthentication()
+                .passwordEncoder(new BCryptPasswordEncoder())
                 .withUser("admin")
-                .password(encoder().encode("password"))
+                .password(new BCryptPasswordEncoder().encode("password"))
                 .roles("USER");
     }
 
-    @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
-    }
+
 }
